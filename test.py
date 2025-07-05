d61d16e382a2bfb86dc1e90086a50cb42bf84b87
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2025-07-05 11:55:03 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+import openpyxl
+from pathlib import Path
+import streamlit as st
+
+# Streamlit Page Config
+st.title("Test App")
+st.write("If you see this, it works.")
